055bf820184d73f2063dd926109dc783bd704b06
xbatm <xbatm@MSI>
2025-01-20 18:54:37 -0600

    Add project files.

---
+++ Wk1Ex11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk1Ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INFOFORMATION Input");
+            Console.WriteLine("What is your first name?");
+            Console.WriteLine("What is your last name?");
+            Console.WriteLine("What is your age?");
+            Console.WriteLine("What is your favorite color");
+        }
+    }
+}
